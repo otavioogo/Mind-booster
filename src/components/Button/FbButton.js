afc69f40318650b5ec69fea8803ba7cacd5bd8d0
Otavio Ogo <79700599+otavioogo@users.noreply.github.com>
2022-05-02 02:21:46 -0300

    Create FbButton.js

---
+++ src/components/Button/FbButton.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet,TouchableWithoutFeedback, Animated } from 'react-native'
+import { AntDesign, Entypo } from '@expo/vector-icons'
+import Cartao from "../../pages/home/Cartao";
+import 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function FbButton(props) {
+
+
+
+     const navigation = useNavigation();
+
+    
+    
+        return (
+            <View style={styles.container}>
+           
+
+
+      
+            <TouchableWithoutFeedback onPress = {() => navigation.navigate("Cartao")}>
+            <Animated.View style={[styles.button, styles.menu]}>
+            <AntDesign name="plus" size={24} color="#FFF" />
+             </Animated.View>
+            </TouchableWithoutFeedback>
+
+            </View>
+        );
+    }
+
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems: "center",
+        position: "absolute",
+        marginLeft: 350,
+        marginTop: 550
+    },
+    button:{
+        
+        position: "absolute",
+        width: 60,
+        height: 60,
+        borderRadius: 60/2,
+        justifyContent: "center",
+        alignItems: "center",
+        shadowRadius: 10,
+        shadowColor: '#857DB1',
+        shadowOpacity: 0.3,
+        shadowOffset: {
+            height: 10
+        }
+    },
+    menu:{
+        backgroundColor: '#857DB1',
+        
+
+    },
+    submenu: {
+        width: 48,
+        height: 48,
+        borderRadius: 48/2,
+        backgroundColor: '#857DB1'
+    }
+});
